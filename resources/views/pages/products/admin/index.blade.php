40ee2943167a7734fa3aaec6f21e05ca84bb60fc
Tacs <act.rtacangjr@gmail.com>
2026-07-28 19:51:29 +0800

    fix: merge conflict

--- resources/views/pages/products/admin/index.blade.php
+++ resources/views/pages/products/admin/index.blade.php
@@ -1,19 +1,10 @@
 <x-layouts.admin>
-<<<<<<< HEAD
     @include('pages.products.admin.index._product_header')
     <div class="overflow-hidden">
         <div class="overflow-x-auto">
             <table class="min-w-full divide-y divide-gray-200">
                 @include('pages.products.admin.index._table_head')
                 @include('pages.products.admin.index._table_body')
-=======
-    @include('pages.products.admin._product_header')
-    <div class="overflow-hidden">
-        <div class="overflow-x-auto">
-            <table class="min-w-full divide-y divide-gray-200">
-                @include('pages.products.admin._table_head')
-                @include('pages.products.admin._table_body')
->>>>>>> b98bfc0aa2c6812f59f6d2e25d83ed2a3d543d20
             </table>
         </div>
     </div>
