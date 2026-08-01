f59e044cdbbff55128539de74350fcee5fc906f2
Tacs <act.rtacangjr@gmail.com>
2026-08-01 20:46:22 +0800

    refactor: added admin in n layer product

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -2,7 +2,9 @@
 
 namespace App\Services;
 
+use App\Models\Product;
 use App\Repositories\ProductRepository;
+use Illuminate\Support\Facades\Storage;
 
 class ProductService
 {
@@ -11,5 +13,31 @@ class ProductService
     public function getAllProducts(?string $search)
     {
         return $this->productRepository->getAllProducts($search);
+    }
+
+    public function createProduct($image, array $validated)
+    {
+        $path = $image->store('products', 'public');
+        $this->productRepository->createProduct($path, $validated);
+    }
+
+    public function updateProduct(Product $product, $image, array $validated)
+    {
+        if ($image) {
+            if ($product->image_url && Storage::disk('public')->exists($product->image_url)) {
+                Storage::disk('public')->delete($product->image_url);
+            }
+
+            $validated['image_url'] = $image->store('products', 'public');
+        }
+        
+        $this->productRepository->updateProduct($product->id, $validated);
+    }
+
+    public function deleteProduct(Product $product) 
+    {
+        Storage::disk('public')->delete($product->image_url);
+
+        $this->productRepository->deleteProduct($product->id);
     }
 }
